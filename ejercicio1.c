12e6ebfd386527b3609612ed461d4c00176ad41a
EAncoHuaracha <eancohu@unsa.edu.pe>
2022-06-26 11:06:29 -0500

    se probo las funciones bis y bic

--- ejercicio1.c
+++ ejercicio1.c
@@ -10,12 +10,18 @@ int bic(int x, int m){
 
 /* Calcular x|y usando solo llamadas a las funciones bis y bic */
 int bool_or(int x, int y) {
-	int resultado = __________ ;
+	int resultado  ;
 	return resultado;
 }
 
 /* Calcula x^y usando solo llamadas a las funciones bis y bic */
 int bool_xor(int x, int y) {
-	int resultado = __________;
+	int resultado ;
 	return resultado;
+}
+
+int main(){
+	int x = 5, m = 1;
+	printf("%d bis: %d \n", x,bis(x,m));
+	printf("%d bic: %d \n", x,bic(x,m));
 }
