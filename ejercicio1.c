d8dea019df35ba4591a34f24d5c27af6a6f1ac50
EAncoHuaracha <eancohu@unsa.edu.pe>
2022-06-26 10:40:34 -0500

    se completaron las funciones bis y bic

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,5 +1,12 @@
-int bis(int x, int m);
-int bic(int x, int m);
+#include <stdio.h>
+
+int bis(int x, int m){
+	return (x | (1 << (m - 1)));
+}
+
+int bic(int x, int m){
+	return (x & (~(1 << (m -1))));
+}
 
 /* Calcular x|y usando solo llamadas a las funciones bis y bic */
 int bool_or(int x, int y) {
